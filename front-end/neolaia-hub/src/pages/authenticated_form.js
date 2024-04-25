b7e21db22243fc1058bcc8cd5cdbca3b6e1c7e65
Gabriele <gabrieletuozzo@gmail.com>
2024-04-25 17:27:21 +0000

    added client side token check and mapping_researchers form rendering

--- front-end/neolaia-hub/src/pages/authenticated_form.js
+++ front-end/neolaia-hub/src/pages/authenticated_form.js
@@ -1,29 +1,36 @@
 import { useContext, useEffect, useState } from "react"; 
 import { AuthContext } from "../components/AuthContext";
 import Auth from "../components/auth";
+import token_is_valid from "../utils";
+import MappingResearchers from "../forms/mapping_researchers";
+
 
 function AuthenticatedForm(){
     const { token, loading} = useContext(AuthContext);  
     const [form, setForm] = useState(null);
+    const [token_validity, setTokenValidity] = useState(false)
 
     useEffect(() => {
         if (loading){
             return 
         }
+        //check on the client side if the token is expired
+        setTokenValidity(token_is_valid())
 
-        if(token){
+        if(token && token_validity){
+            //insert here the form component to render after login
             const form_component = (
-                <p>Compila il form</p>
+                <MappingResearchers />
             )
             setForm(form_component)
         }
-    }, [loading, token])
+    }, [loading, token, token_validity])
     
 
     return(
         <div>
             {loading && <p>Caricamento...</p>}
-            {!token && <Auth />}
+            {(!token || !token_validity) && <Auth />}
             {token && form}
         </div>
     )
