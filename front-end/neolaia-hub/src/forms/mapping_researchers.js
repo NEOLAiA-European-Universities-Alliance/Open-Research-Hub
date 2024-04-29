08cc57d83b0d901014cc609f98093867e7101cb7
Gabriele <gabrieletuozzo@gmail.com>
2024-04-29 11:13:46 +0000

    fix: questions already answered from the previous panel

--- front-end/neolaia-hub/src/forms/mapping_researchers.js
+++ front-end/neolaia-hub/src/forms/mapping_researchers.js
@@ -4,12 +4,19 @@ import personal_info from '../components/form_components/personal_info.json';
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 
+const erc_panel_interest = JSON.parse(JSON.stringify(erc_panel));
+erc_panel_interest.pages[0].elements[0].title = 'Select 3 areas of research in which you are interested'
+
+//To solve question already answered from the research are panel, rename all key 
+erc_panel_interest.name = 'erc_panel_research_area_interested'
+erc_panel_interest.pages[0].name= 'interested_research_area'
+erc_panel_interest.pages[0].elements[0].name = 'ERC_panel_interested'
+
 personal_info.pages.push(erc_panel.pages[0])
+personal_info.pages.push(erc_panel_interest.pages[0])
 
 function MappingResearchers(){
     const research_survey = new Model(personal_info)
-
-    //const research_survey = new Model(erc_panel)
 
     return <Survey model={research_survey} />
 }
