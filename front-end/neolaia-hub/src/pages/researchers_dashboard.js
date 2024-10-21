12d8fbe49a1de2c9d5b2d0b04db29aca5ba6fb75
Gabriele <gabrieletuozzo@gmail.com>
2024-10-21 10:44:50 +0000

    updated name of the project from NEOLAiA surveys to Open Research Hub

--- front-end/neolaia-hub/src/pages/researchers_dashboard.js
+++ front-end/neolaia-hub/src/pages/researchers_dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import logo_neolaia from '../img/logoNEOLAiA.png'
 import logo_eu from '../img/eu_logo.png'
 import { Link } from 'react-router-dom';
@@ -16,7 +16,7 @@ function ResearcherDash(){
             <Container fluid>
                 <Row id='welcome-msg'>
                     <p>
-                        <h1 className='h1'>Welcome to Research Survey</h1>
+                        <h1 className='h1'>Welcome to the Open Research Hub</h1>
                         <img src={logo_neolaia} alt='Logo NEOLAiA' className="img-fluid" id='neolaia-logo'></img>
                         <img src={logo_eu} alt='Logo EU' className="img-fluid" id='eu-logo'></img>
                     </p>
@@ -24,10 +24,10 @@ function ResearcherDash(){
                 <Row>
                     <Col md={12} className='text-center'>
                         <h5 id='welcome-text'>
-                            Welcome to the survey of researchers in NEOLAiA. We are asking to contribute to the mapping of Research in NEOLAiA providing information that will be openly accessible (with license CC-BY-SA) within and outside NEOLAiA to foster networking and stimulate collaboration. 
+                            Welcome to the Open Research Hub. We are asking to contribute to the mapping of Research in NEOLAiA providing information that will be openly accessible (with license CC-BY-SA) within and outside NEOLAiA to foster networking and stimulate collaboration. 
                             This is only an early prototype that we are testing, and you will be also asked to fill in a very short feedback form. In case you need support, please write to <a href='mailto:neolaiasurvey-support-list@unisa.it'>neolaiasurvey-support-list@unisa.it</a>
                         </h5>
-                        <Button as={Link} to="/researchers" id='start-btn'>Start/Edit Survey</Button>
+                        <Button as={Link} to="/survey" id='start-btn'>Start/Edit Survey</Button>
                         <Button variant='secondary' as={Link} to="/search-researchers" id='search-btn'>Search</Button>
                     </Col>
                 </Row>
