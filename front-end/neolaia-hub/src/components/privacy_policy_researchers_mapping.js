44dd9e03a19f66047421562d285a42b65e2fbdc3
Gabriele <gabrieletuozzo@gmail.com>
2024-12-04 12:04:51 +0000

    updated the privacy policy text

--- front-end/neolaia-hub/src/components/privacy_policy_researchers_mapping.js
+++ front-end/neolaia-hub/src/components/privacy_policy_researchers_mapping.js
@@ -5,10 +5,9 @@ const PrivacyMessageResearchersMapping = () => {
         <>
         <p style={{textAlign:"left", marginLeft: "25px", marginRight: "20px"}}>
             Dear Professor/Researcher, <br />
-            Thank you for your interest in completing our NEOLAiA survey. This will help you to find colleagues with similar research areas and interests to start collaborative projects and increase research synergies.  
-            Please read this short guide before you start. Once you have all the necessary information, the survey will take between 3 and 5 minutes to complete, including the feedback you will provide
-
-            This is what we will ask you:
+            Thank you for your interest in participating in our NEOLAiA Research Hub. This will help you to find colleagues with similar research areas and interests to start collaborative projects and increase research synergies.
+            Please read this short guide before you start so that you can prepare the information needed and speed up.
+            Once you have all the necessary information, filling up the form will take between 3 and 5 minutes to complete. This is what we will ask you:
             <ul>
             <li>Your first and last name </li>
             <li>Your university and affiliation </li>
@@ -22,11 +21,11 @@ const PrivacyMessageResearchersMapping = () => {
                 <li>refer to the same ERC list as above</li>
             </ul>
             <li>Your ORCID profile </li>
-            <li>A set of free keywords related to your research (you can choose from existing words or create new ones: as you type, existing keywords will be presented to you)  </li>
+            <li>A set of 10 free keywords related to your research (you can choose from existing words or create new ones: as you type, existing keywords will be presented to you)</li>
             </ul>
             
-            Please note that the data you provide will be used as Open Data, with the license CC-BY-SA (ATTRIBUTION-SHAREALIKE 4.0 INTERNATIONAL) and you authorise us to use and show it on Neolaia platform. 
-
+            Please note that the data you provide will be used as Open Data, with the license CC-BY-SA (ATTRIBUTION-SHAREALIKE 4.0 INTERNATIONAL) and, by ticking the boxes below, you are authorising us to use and show it on NEOLAiA Research Hub.
+            You will be asked accept the boxes below and to write your email (in one of the NEOLAiA partner universities) and you will be sent a One Time Password (OTP) that will last for an hour.
             </p>
         </>
     );
